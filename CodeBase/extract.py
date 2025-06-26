dc935cc16f2cf457cc8b235e32b01ff6500f32fa
Chetan <cemorkhade@gmail.com>
2025-06-26 15:40:18 +0530

    files added with code changed for extraction

--- CodeBase/extract.py
+++ CodeBase/extract.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from sqlalchemy import create_engine
-import cx_Oracle
+import oracledb
 import logging
+import os
+from pathlib import Path
 
-oracle_engine = create_engine("oracle+cx_oracle://system:admin@localhost:1521/xe")
-mysql_engine = create_engine("mysql+pymysql://root:Admin%40143@localhost:3308/stag_retaildwg")
+oracledb.init_oracle_client(lib_dir=r"C:\oracle\instantclient_19_11\instantclient-basic-windows.x64-23.8.0.25.04\instantclient_23_8")
+oracle_engine = create_engine("oracle+oracledb://SYSTEM:SYS@localhost:1521/xe")
+mysql_engine = create_engine("mysql+pymysql://root:root@localhost:3306/stag_retaildwh")
 
 logging.basicConfig(
     filename='LogFiles/Extraction.log',
@@ -18,7 +21,7 @@ logger = logging.getLogger(__name__)
 def extract_sales_data_from_file():
     try:
         logger.info("sales data extraction started....")
-        df = pd.read_csv("SourceSystems/sales_data_Linux_remot.csv")
+        df = pd.read_csv("SourceSystems/sales_data_Linux_remote.csv")
         df.to_sql("staging_sales",mysql_engine,if_exists='replace',index=False)
         logger.info("sales data extraction completed....")
     except Exception as e:
@@ -28,7 +31,7 @@ def extract_sales_data_from_file():
 def extract_product_data_from_file():
     try:
         logger.info("product data extraction started....")
-        df = pd.read_csv("SourceSystems/product_data.cs")
+        df = pd.read_csv("SourceSystems/product_data.csv")
         df.to_sql("staging_product",mysql_engine,if_exists='replace',index=False)
         logger.info("product data extraction completed....")
     except Exception as e:
@@ -47,8 +50,8 @@ def extract_supplier_data_from_file():
 def extract_inventory_data_from_file():
     try:
         logger.info("inventory data extraction started....")
-        df = pd.read_xml("SourceSystems/inventory_data.xml",xpath=".//item")
-        df.to_sql("staging_supplier",mysql_engine,if_exists='replace',index=False)
+        df = pd.read_xml("SourceSystems/inventory_data.xml", xpath=".//item")
+        df.to_sql("staging_inventory", mysql_engine, if_exists='replace', index=False)
         logger.info("inventory data extraction completed....")
     except Exception as e:
         logger.error(f"Error encounted while extrcting the inventory data file,{e}",exc_info=True)
